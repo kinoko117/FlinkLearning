47bac35eeb8fae0f9c065735cf967880f0f637e5
zhiling <zhiling@atguigu.cn>
2022-02-26 15:04:08 +0800

    update

---
+++ src/main/java/com/atguigu/bean/Person.java
@@ -0,0 +1,11 @@
+package com.atguigu.bean;
+
+/**
+ * @Author lizhenchao@atguigu.cn
+ * @Date 2022/2/26 15:03
+ */
+public class Person {
+    public static void main(String[] args) {
+        System.out.println("xxxx");
+    }
+}

183afeba7697a2df1457a77aedd2943bca07b5cb
zhiling <zhiling@atguigu.cn>
2022-03-01 16:20:20 +0800

    update

---
+++ src/main/java/com/atguigu/flink/chapter05/transform/Flink06_Rebalance.java
@@ -0,0 +1,34 @@
+package com.atguigu.flink.chapter05.transform;
+
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+/**
+ * @Author lizhenchao@atguigu.cn
+ * @Date 2022/3/1 14:52
+ */
+public class Flink06_Rebalance {
+    public static void main(String[] args) {
+        Configuration conf = new Configuration();
+        conf.setInteger("rest.port", 20000);
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment(conf);
+        env.setParallelism(2);
+        
+        DataStreamSource<Integer> stream = env.fromElements(1, 2, 3, 4, 5, 6);
+        stream
+            .rebalance()  // 对数据做平均分布
+            .print();
+        
+        try {
+            env.execute();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+/*
+
+
+
+ */
